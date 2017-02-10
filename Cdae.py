9a782922efbee607cd7d244e9effc9b22180e44b
dawnknight <atc327@nyu.edu>
2017-02-09 23:11:33 -0500

    add

---
+++ Cdae.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 08 23:31:40 2017
+
+@author: Dawnknight
+"""
+import tensorflow as tf
+import numpy as np
+import h5py
+from random import shuffle as sf
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding = 'SAME')
+
+def deconv2d(x, W, output_shape):
+    return tf.nn.conv2d_transpose(x, W, output_shape, strides = [1, 2, 2, 1], padding = 'SAME')
+
+    
+
+# set a placeholder for future input
+x = tf.placeholder(tf.float32, shape = [None, 30,33])
+x_noise = tf.placeholder(tf.float32, shape = [None, 30,33])    
+
+x_origin = tf.reshape(x, [-1, 30, 33, 1])
+x_origin_noise = tf.reshape(x_noise, [-1, 30, 33, 1])
+    
+    
+# initialize weight
+W_e_init1 = tf.truncated_normal([5, 5, 1, 16], mean=0.0, stddev=0.01)
+W_e_init2 = tf.truncated_normal([5, 5, 16,32], mean=0.0, stddev=0.01)
+W_d_init1 = tf.truncated_normal([5, 5, 16,32], mean=0.0, stddev=0.01)
+W_d_init2 = tf.truncated_normal([5, 5, 1, 16], mean=0.0, stddev=0.01)
+    
+
+#encoder
+
+W_e_conv1 = tf.Variable(W_e_init1 , name = 'W_e_1')
+b_e_conv1 = tf.Variable(tf.constant(0.1,shape = [16]), name='b_e_1')
+h_e_conv1 = tf.nn.relu(tf.add(conv2d(x_origin_noise, W_e_conv1), b_e_conv1))
+
+W_e_conv2 = tf.Variable(W_e_init2 , name = 'W_e_2')
+b_e_conv2 = tf.Variable(tf.constant(0.1,shape = [32]), name='b_e_2')
+h_e_conv2 = tf.nn.relu(tf.add(conv2d(h_e_conv1, W_e_conv2), b_e_conv2))
+
+code_layer = h_e_conv2
+print("code layer shape : %s" % h_e_conv2.get_shape())
+
+
+#decoder
+
+W_d_conv1 = tf.Variable(W_d_init1 , name = 'W_d_1')
+b_d_conv1 = tf.Variable(tf.constant(0.1,shape = [32]), name='b_d_1')
+output_shape_d_conv1 = tf.pack([tf.shape(x)[0], 15, 17, 16])
+h_d_conv1 = tf.nn.relu(deconv2d(tf.add(h_e_conv2,b_d_conv1), W_d_conv1,output_shape_d_conv1))
+
+#h_d_conv1 = tf.nn.relu(tf.add(conv2d((h_e_conv2, W_d_conv1,output_shape_d_conv1), b_d_conv1)))
+
+W_d_conv2 = tf.Variable(W_d_init2 , name = 'W_d_2')
+b_d_conv2 = tf.Variable(tf.constant(0.1,shape = [16]), name='b_d_2')
+output_shape_d_conv2 = tf.pack([tf.shape(x)[0], 30, 33,1])
+h_d_conv2 = tf.nn.relu(deconv2d(tf.add(h_d_conv1,b_d_conv2), W_d_conv2,output_shape_d_conv2))
+#h_d_conv2 = tf.nn.relu(tf.add(deconv2d((h_d_conv1, W_d_conv2,output_shape_d_conv1), b_d_conv2)))
+
+x_reconstruct = h_d_conv2[:,:,:,0]
+print("reconstruct layer shape : %s" % x_reconstruct.get_shape())
+
+cost = tf.reduce_mean(tf.pow(x_reconstruct - x, 2))
+optimizer = tf.train.AdamOptimizer(0.00001).minimize(cost)
+
+sess = tf.InteractiveSession()
+batch_size = 16
+counter = 0
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+
+f_test   = h5py.File('Ndata_batch.h5','r')['test_data'][:].T
+f_telab  = h5py.File('Ndata_batch.h5','r')['test_label'][:].T
+f_train  = h5py.File('Ndata_batch.h5','r')['train_data'][:].T
+f_trlab  = h5py.File('Ndata_batch.h5','r')['train_label'][:].T
+minmax   = h5py.File('Ndata_batch.h5','r')['minmax'][:]
+
+idx = np.arange(f_train.shape[0])
+sf(idx)
+
+for epoch in range(30000):
+        
+    if (counter+1)*batch_size >f_train.shape[0]:
+        counter = 0
+        idx = np.arange(f_train.shape[0])
+        sf(idx)
+        
+    batch_raw = f_trlab[idx[(counter)*batch_size:(counter+1)*batch_size],:,:]
+    batch_noise =f_train[idx[(counter)*batch_size:(counter+1)*batch_size],:,:]
+                             
+    if epoch < 1500:
+        if epoch%100 == 0:
+            print("step %d, loss %g"%(epoch, cost.eval(feed_dict={x:batch_raw, x_noise: batch_noise})))
+    else:
+        if epoch%1000 == 0: 
+            print("step %d, loss %g"%(epoch, cost.eval(feed_dict={x:batch_raw, x_noise: batch_noise})))
+    
+    optimizer.run(feed_dict={x:batch_raw, x_noise: batch_noise})
+
+print(sess.run(cost, feed_dict={x: f_telab, x_noise: f_test}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tensorgen.py
+++ tensorgen.py
@@ -4,27 +4,74 @@ Created on Wed Feb 08 17:16:17 2017
 
 @author: medialab
 """
-import os , glob , cPickle,copy
+import os , glob , cPickle , h5py
 import numpy as np
+from random import shuffle as sf
 
 
-
-path = 'D:/Project/PyKinect2-master/Kproject/data/Motion and Kinect/'
+path = 'C:/Users/Dawnknight/Documents/GitHub/Kproject/data/Motion and Kinect/'
 
 Kfolder = 'Unified_KData/'
 Mfolder = 'Unified_MData/'
 
 batchsize = 30 # group of joints
 jnum = 33
+index = 0
 
-for kinfile,minfile in zip(glob.glob(os.path.join(path+Kfolder,'*ex4.pkl')),glob.glob(os.path.join(path+Mfolder,'*FPS30_motion_unified_ex4.pkl'))):
+#st = time.clock()
+
+#for kinfile,minfile in zip(glob.glob(os.path.join(path+Kfolder,'*ex4.pkl')),glob.glob(os.path.join(path+Mfolder,'*FPS30_motion_unified_ex4.pkl'))):
+#    print('group_'+str(index+1)+'......')
 #    print  kinfile
 #    print  minfile  
-    kbatch = np.zeros([jnum,batchsize])
-    mbatch = np.zeros([jnum,batchsize])
+#    print('==================================\n\n\n\n\n')
+#
+#    kdata = cPickle.load(file(kinfile,'rb'))
+#    mdata = cPickle.load(file(minfile,'rb'))
+#    length = min(kdata[0].shape[1],mdata[0].shape[1])
+#    for i in kdata.keys():
+#        if i == 0:
+#            Kjoints = kdata[i]
+#            Mjoints = mdata[i]
+#        else:
+#            Kjoints = np.vstack([Kjoints,kdata[i]])
+#            Mjoints = np.vstack([Mjoints,mdata[i]])
+##    pdb.set_trace()        
+#    for idx,i in enumerate(xrange(batchsize-1,length)):
+#        
+#        if idx == 0:
+#            Ksubtensor = copy.copy(Kjoints[:,idx:batchsize+idx])
+#            Msubtensor = copy.copy(Mjoints[:,idx:batchsize+idx])
+#        else: 
+#            Ksubtensor = np.dstack([Ksubtensor,Kjoints[:,idx:batchsize+idx]])
+#            Msubtensor = np.dstack([Msubtensor,Mjoints[:,idx:batchsize+idx]])
+##    pdb.set_trace()
+#    if index == 0:
+#        Ktensor = Ksubtensor
+#        Mtensor = Msubtensor
+#    else:
+#        Ktensor = np.dstack([Ktensor,Ksubtensor])
+#        Mtensor = np.dstack([Mtensor,Msubtensor]) 
+#    index += 1
+#    
+#print time.clock()-st
+
+LEN = []
+Ksubtensor = {}
+Msubtensor = {}    
+for kinfile,minfile in zip(glob.glob(os.path.join(path+Kfolder,'*ex4.pkl')),glob.glob(os.path.join(path+Mfolder,'*FPS30_motion_unified_ex4.pkl'))):
+    print('group_'+str(index+1)+'......')
+    print  kinfile
+    print  minfile  
+    print('==================================\n\n\n')
+
     kdata = cPickle.load(file(kinfile,'rb'))
     mdata = cPickle.load(file(minfile,'rb'))
-    length = min(kdata[0].shape[1],kdata[0].shape[1])
+    length = min(kdata[0].shape[1],mdata[0].shape[1])
+    LEN.append(length-batchsize+1) 
+    Ksubtensor[index] = np.zeros([jnum,batchsize,length-batchsize+1])
+    Msubtensor[index] = np.zeros([jnum,batchsize,length-batchsize+1])
+    
     for i in kdata.keys():
         if i == 0:
             Kjoints = kdata[i]
@@ -32,13 +79,49 @@ for kinfile,minfile in zip(glob.glob(os.path.join(path+Kfolder,'*ex4.pkl')),glob
         else:
             Kjoints = np.vstack([Kjoints,kdata[i]])
             Mjoints = np.vstack([Mjoints,mdata[i]])
+        
     for idx,i in enumerate(xrange(batchsize-1,length)):
         
-        if idx == 0:
-           kbatch[:] = copy.copy(Kjoints[:,idx:batchsize+idx]) 
-           mbatch[:] = copy.copy(Mjoints[:,idx:batchsize+idx]) 
+            Ksubtensor[index][:,:,idx] = Kjoints[:,idx:batchsize+idx]
+            Msubtensor[index][:,:,idx] = Mjoints[:,idx:batchsize+idx]
+    index += 1
+    
+Ktensor = np.zeros([jnum,batchsize,sum(LEN)])
+Mtensor = np.zeros([jnum,batchsize,sum(LEN)])
+start = 0
+end   = 0
+for i in range(index-1):
+        end += LEN[i]
+        Ktensor[:,:,start:end] = Ksubtensor[i]
+        Mtensor[:,:,start:end] = Msubtensor[i]
+        start = end
 
-            
+
+idx = range(sum(LEN))
+testrate = 0.2
+sf(idx)
+
+MAX = np.max([Ktensor.max(),Mtensor.max()])
+MIN = np.min([Ktensor.min(),Mtensor.min()])
+
+#normalized to 0 to 1
+NK = (Ktensor-MIN)/(MAX-MIN)
+NM = (Mtensor-MIN)/(MAX-MIN)
+
+teX   = NK[:,:,:int(0.2*sum(LEN))]
+teL   = NM[:,:,:int(0.2*sum(LEN))]
+trX   = NK[:,:,int(0.2*sum(LEN)):]
+trL   = NM[:,:,int(0.2*sum(LEN)):] 
+
+f = h5py.File("Ndata_batch.h5", "w")
+f.create_dataset('train_data' , data = trX) 
+f.create_dataset('train_label', data = trL) 
+f.create_dataset('test_data'  , data = teX) 
+f.create_dataset('test_label' , data = teL) 
+f.create_dataset('idx'        , data = idx) 
+f.create_dataset('minmax'     , data =[MIN,MAX]) 
+f.close()             
+        
             
             
             
