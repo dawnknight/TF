0ab9649aa53493ac1eb0e8894850972457c0ac81
dawnknight <atc327@nyu.edu>
2017-07-07 16:37:52 -0400

    add

--- GPR_w_relmod.py
+++ GPR_w_relmod.py
@@ -7,61 +7,71 @@ Created on Thu Jul 06 15:48:10 2017
 
 
 #import h5py,glob,os,pdb
-#import numpy as np
-#from sklearn.gaussian_process import GaussianProcessRegressor
-#from sklearn.gaussian_process.kernels \
-#    import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
-#from sklearn.datasets import fetch_mldata
-#try :
-#    import cPickle
-#except:
-#    import _pickle as cPickle
-#
-#from sklearn.externals import joblib    
-#    
-#[MIN,MAX] = h5py.File('./data/CNN/model_CNN_0521_K2M_rel.h5','r')['minmax'][:]
-#
-##src_path  = 'I:/AllData_0327/'
-#src_path  = 'D:/Project/K_project/data/'
-#outfolder   = 'unified data array/Unified_MData/'
-#Infolder  = 'unified data array/Unified_KData/'
-#Rfolder   = 'unified data array/reliability_mod/'
-#gprfolder = 'GPR_K2M_mod_rel/'
-#
-#Rel_th    =  0.7
-#
-#k1 = 66.0**2 * RBF(length_scale=67.0)  # long term smooth rising trend
-#
-#    
-#k4 = 0.18**2 * RBF(length_scale=0.134) \
-#    + WhiteKernel(noise_level=0.19**2)  # noise terms
-#    
-#kernel_gpml = k1 + k4
-#
-#
-#gp = GaussianProcessRegressor(kernel=kernel_gpml)
-#
-#
-#M_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_M'].T
-#K_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_K'].T
-#
-#        
-#M  = (M_train_rel[:15000,:]-MIN)/(MAX-MIN) 
-#K  = (K_train_rel[:15000,:]-MIN)/(MAX-MIN) 
-#
-#print('training ....')
-#gp.fit(K, M)
-#
-#print('training finish....')
-#
-#joblib.dump(gp,src_path+gprfolder+'GP_model_0707.pkl')
-#
-#print('model saved....')
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels \
+    import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
+from sklearn.datasets import fetch_mldata
+try :
+    import cPickle
+except:
+    import _pickle as cPickle
+
+from sklearn.externals import joblib 
+import glob,os,h5py   
+    
+[MIN,MAX] = h5py.File('./data/CNN/model_CNN_0521_K2M_rel.h5','r')['minmax'][:]
+
+#src_path  = 'I:/AllData_0327/'
+src_path  = 'D:/Project/K_project/data/'
+outfolder   = 'unified data array/Unified_MData/'
+Infolder  = 'unified data array/Unified_KData/'
+Rfolder   = 'unified data array/reliability_mod/'
+gprfolder = 'GPR_K2M_mod_rel/'
+
+Rel_th    =  0.7
+factor = 5
+
+def uni_vec(Body):
+    vec = np.roll(Body,-3,axis = 0)-Body
+
+    tmp = ((vec**2).reshape(-1,3,vec.shape[1]).sum(axis=1))**.5
+    vlen = np.insert(np.insert(tmp,np.arange(6),tmp,0),np.arange(0,12,2),tmp,0)
+
+    return vec/vlen
+
+
+k1 = 66.0**2 * RBF(length_scale=67.0)  # long term smooth rising trend
+
+    
+k4 = 0.18**2 * RBF(length_scale=0.134) \
+    + WhiteKernel(noise_level=0.19**2)  # noise terms
+    
+kernel_gpml = k1 + k4
+
+
+gp = GaussianProcessRegressor(kernel=kernel_gpml)
+
+
+M_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_M'][12:30].T
+K_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_K'][12:30].T
+
+        
+M  = (M_train_rel[:15000,:]-MIN)/(MAX-MIN) 
+K  = (K_train_rel[:15000,:]-MIN)/(MAX-MIN) 
+
+print('training ....')
+gp.fit(K, M)
+
+print('training finish....')
+
+joblib.dump(gp,src_path+gprfolder+'GP_model_0707_18j.pkl')
+
+print('model saved....')
 
 #gp = joblib.load(src_path+gprfolder+'GP_model_0625.pkl')
 # =======================================
 
-#gp = cPickle.load(file(src_path+gprfolder+'GP_model_0524.pkl','rb'))
 
 Err = 0
 Err_unrel = 0
@@ -83,16 +93,36 @@ for Infile,outfile,Rfile in zip(glob.glob(os.path.join(src_path+Infolder,'*.pkl'
     Mgpr    = np.zeros((18,Indata.shape[1]))
     print(Infile)
     
-    for ii in range(Len):
+#    for ii in range(Len):
+#        
+#        Mpred = gp.predict(Indata[:,ii].reshape((-1,18)))
+#        Mgpr[:,ii] = Mpred[0,:]*(MAX-MIN)+MIN
+
         
-        Mpred = gp.predict(Indata[:,ii].reshape((-1,18)))
-        Mgpr[:,ii] = Mpred[0,:]*(MAX-MIN)+MIN
+    Mpred = gp.predict(Indata.T)
+    Mgpr[:] = Mpred.T*(MAX-MIN)+MIN
+    uni_data = np.zeros(Mgpr.shape) 
+    univec   = uni_vec(Mgpr)
+    
+    uni_data[0:3  ,:] = Mgpr[0:3  ,:]
+    uni_data[9:12 ,:] = Mgpr[9:12 ,:]
+
+    uni_data[3:6  ,:] = Mgpr[0:3  ,:]+univec[0:3  ,:]*33.2*factor
+    uni_data[6:9  ,:] = Mgpr[3:6  ,:]+univec[3:6  ,:]*27.1*factor
+    uni_data[12:15,:] = Mgpr[9:12 ,:]+univec[9:12 ,:]*33.2*factor
+    uni_data[15:  ,:] = Mgpr[12:15,:]+univec[12:15,:]*27.1*factor    
+    
+
     
 #    Err = Err + np.sum(abs(Mgpr[:,:Len]-outdata[:,:Len]))    
 #    Err_unrel + np.sum(abs((Mgpr[:,:Len]-outdata[:,:Len])[unrel_idx]))
 
-    Err = Err + np.sum(np.sum(((Mgpr[:,:Len]-outdata[:,:Len]).reshape(-1,3,Len))**2,axis=1)**0.5)    
-    Err_unrel + np.sum(np.sum((((Mgpr[:,:Len]-outdata[:,:Len])[unrel_idx]).reshape(-1,3,Len))**2,axis=1)**0.5)
+    
+
+
+
+    Err = Err + np.sum(np.sum(((uni_data[:,:Len]-outdata[:,:Len]).reshape(-1,3,Len))**2,axis=1)**0.5)    
+    Err_unrel + np.sum(np.sum((((uni_data[:,:Len]-outdata[:,:Len])[unrel_idx]).reshape(-1,3,Len))**2,axis=1)**0.5)
 
 
     fname = src_path+gprfolder+Infile.split('\\')[-1][:-3]+'h5'
