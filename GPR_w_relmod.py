4cf388cabb2f550ef1b86b4dd2e8fd883b4746cc
dawnknight <atc327@nyu.edu>
2017-07-07 16:20:47 -0400

    add

--- GPR_w_relmod.py
+++ GPR_w_relmod.py
@@ -6,57 +6,57 @@ Created on Thu Jul 06 15:48:10 2017
 """
 
 
-import h5py,glob,os,pdb
-import numpy as np
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels \
-    import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
-from sklearn.datasets import fetch_mldata
-try :
-    import cPickle
-except:
-    import _pickle as cPickle
-
-from sklearn.externals import joblib    
-    
-[MIN,MAX] = h5py.File('./data/CNN/model_CNN_0521_K2M_rel.h5','r')['minmax'][:]
-
-#src_path  = 'I:/AllData_0327/'
-src_path  = 'D:/Project/K_project/data/'
-outfolder   = 'unified data array/Unified_MData/'
-Infolder  = 'unified data array/Unified_KData/'
-Rfolder   = 'unified data array/reliability_mod/'
-gprfolder = 'GPR_K2M_mod_rel/'
-
-Rel_th    =  0.7
-
-k1 = 66.0**2 * RBF(length_scale=67.0)  # long term smooth rising trend
-
-    
-k4 = 0.18**2 * RBF(length_scale=0.134) \
-    + WhiteKernel(noise_level=0.19**2)  # noise terms
-    
-kernel_gpml = k1 + k4
-
-
-gp = GaussianProcessRegressor(kernel=kernel_gpml)
-
-
-M_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_M'].T
-K_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_K'].T
-
-        
-M  = (M_train_rel[:15000,:]-MIN)/(MAX-MIN) 
-K  = (K_train_rel[:15000,:]-MIN)/(MAX-MIN) 
-
-print('training ....')
-gp.fit(K, M)
-
-print('training finish....')
-
-joblib.dump(gp,src_path+gprfolder+'GP_model_0707.pkl')
-
-print('model saved....')
+#import h5py,glob,os,pdb
+#import numpy as np
+#from sklearn.gaussian_process import GaussianProcessRegressor
+#from sklearn.gaussian_process.kernels \
+#    import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared
+#from sklearn.datasets import fetch_mldata
+#try :
+#    import cPickle
+#except:
+#    import _pickle as cPickle
+#
+#from sklearn.externals import joblib    
+#    
+#[MIN,MAX] = h5py.File('./data/CNN/model_CNN_0521_K2M_rel.h5','r')['minmax'][:]
+#
+##src_path  = 'I:/AllData_0327/'
+#src_path  = 'D:/Project/K_project/data/'
+#outfolder   = 'unified data array/Unified_MData/'
+#Infolder  = 'unified data array/Unified_KData/'
+#Rfolder   = 'unified data array/reliability_mod/'
+#gprfolder = 'GPR_K2M_mod_rel/'
+#
+#Rel_th    =  0.7
+#
+#k1 = 66.0**2 * RBF(length_scale=67.0)  # long term smooth rising trend
+#
+#    
+#k4 = 0.18**2 * RBF(length_scale=0.134) \
+#    + WhiteKernel(noise_level=0.19**2)  # noise terms
+#    
+#kernel_gpml = k1 + k4
+#
+#
+#gp = GaussianProcessRegressor(kernel=kernel_gpml)
+#
+#
+#M_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_M'].T
+#K_train_rel = cPickle.load(file('GPR_training_testing_set33.pkl','rb'))['Rel_train_K'].T
+#
+#        
+#M  = (M_train_rel[:15000,:]-MIN)/(MAX-MIN) 
+#K  = (K_train_rel[:15000,:]-MIN)/(MAX-MIN) 
+#
+#print('training ....')
+#gp.fit(K, M)
+#
+#print('training finish....')
+#
+#joblib.dump(gp,src_path+gprfolder+'GP_model_0707.pkl')
+#
+#print('model saved....')
 
 #gp = joblib.load(src_path+gprfolder+'GP_model_0625.pkl')
 # =======================================
@@ -70,8 +70,8 @@ for Infile,outfile,Rfile in zip(glob.glob(os.path.join(src_path+Infolder,'*.pkl'
                                 glob.glob(os.path.join(src_path+outfolder,'*ex4_FPS30_motion_unified.pkl')),\
                                  glob.glob(os.path.join(src_path+Rfolder,'*ex4.pkl'))):
      
-    Indata  = (cPickle.load(file(Infile,'rb')) -MIN)/(MAX-MIN)
-    outdata = cPickle.load(file(outfile,'rb'))
+    Indata  = (cPickle.load(file(Infile,'rb'))[12:30,:] -MIN)/(MAX-MIN)
+    outdata = cPickle.load(file(outfile,'rb'))[12:30,:]
     rdata   = cPickle.load(file(Rfile,'rb'))
         
 #    Indata  = (cPickle.load(open(Infile,'rb'),encoding = 'latin1')[12:30,:]-MIN)/(MAX-MIN)
